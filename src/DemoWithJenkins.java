c704cd8179c17c8e9f9b1d3d37292c744f73bb4f
skiranmai <skiranmai78@gmail.com>
2018-04-18 23:30:10 -0700

    small corrections

--- src/DemoWithJenkins.java
+++ src/DemoWithJenkins.java
@@ -13,21 +13,21 @@ public class DemoWithJenkins {
 	@BeforeTest
 	public void setUp() {
 		driver = new FirefoxDriver();
-		System.out.println("after browser");
+		System.out.println("After browser....");
 		driver.get("http://www.google.com");
-		System.out.println("after loading ");
+		System.out.println("After loading.... ");
 		driver.manage().window().maximize();
-		System.out.println("after max");
+		System.out.println("After max.....");
 		driver.manage().timeouts().implicitlyWait(2000, TimeUnit.SECONDS);
-		System.out.println("wait");
+		System.out.println("After wait....");
 	}
 
 	@Test
 	public void demo() {
          String actualTitle = driver.getTitle();
-         System.out.println("title"+actualTitle);
+         System.out.println("Title "+actualTitle);
          Assert.assertEquals(actualTitle, "Google");
-         System.out.println("nnn");
+         System.out.println("After assertion....");
 	}
 
 	@AfterTest
